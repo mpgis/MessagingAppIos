5d978b3615c841181bd749902839442ffec2182b
James <kuba.slusarski@gmail.com>
2021-08-30 22:09:55 +0200

    Added a simple animation to logo in WelcomeViev

--- MessagingApp/Controllers/WelcomeViewController.swift
+++ MessagingApp/Controllers/WelcomeViewController.swift
@@ -10,11 +10,20 @@ import UIKit
 class WelcomeViewController: UIViewController {
 
     @IBOutlet weak var titleLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let titleText = "⚡️FlashChat"
+        titleLabel.text = ""
+        var charIndex = 0.0
+        for letter in titleText {
+            Timer.scheduledTimer(withTimeInterval: 0.1 * charIndex, repeats: false) { (timer) in
+                self.titleLabel.text?.append(letter)
+            }
+            charIndex += 1
+        }
     }
-
 
 }
 
